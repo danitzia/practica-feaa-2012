6e8e00f7e3d52f285d7c94a406dde2c40910b79b
alina91 <alina_u91@yahoo.com>
2012-06-27 13:44:52 +0300

    orice

---
+++ Gestiunea retelei de transport public/FormCategoriiSimplu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Gestiunea_retelei_de_transport_public
+{
+    public partial class FormCategoriiSimplu : Form
+    {
+        public FormCategoriiSimplu()
+        {
+            InitializeComponent();
+        }
+    }
+}
